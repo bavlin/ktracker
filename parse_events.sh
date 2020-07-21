a702d1521c6753b7fde63319d678d975edf3793b
Suraj <me.suraj.pandey@gmail.com>
2020-07-20 21:57:37 -0400

    Trying to parse event conditions

--- parse_events.sh
+++ parse_events.sh
@@ -5,15 +5,43 @@ cur_idx_resource=$3
 resource_name=$4
 event=$5
 
+parse_first_key_value_pair_from_string() {
+  input_string=$1
+  key_value_pair=$(echo $input_string | sed -e "s/&&/&/g" | cut -d '&' -f1)
+  input_string=$(echo $input_string | sed "s/.*=[0-9 ]*[& ]*//g")
+  echo "$input_string"
+}
+
+parse_conditions() {
+  condition_string=$1
+  IFS_BACKUP=$IFS
+  IFS='.'
+  read -a condition_key <<< "$condition_string"
+  IFS=$IFS_BACKUP
+  if [[ ${condition_key[0]} != "event" ]]; then
+    echo "Condition string not correct, exiting.."
+    exit 1
+  fi
+  #walk( if type == "object" then with_entries( select(.key == "a") ) else . end ) | .[0] | length 
+}
+
 #separate the conditions and the result
 IFS_BACKUP=$IFS
 IFS=';'
-read -a events_and_conditions <<< "$event"
+read -a conditions_and_results <<< "$event"
 IFS=$IFS_BACKUP
-events=${events_and_conditions[0]}
-conditions=${events_and_conditions[1]}
-if [[ $events = "" && $conditions = "" ]]; then
-  echo "Events and conditions empty, doin.. nothing"
+conditions=${conditions_and_results[0]}
+results=${conditions_and_results[1]}
+if [[ $conditions = "" && $results = "" ]]; then
+  echo "Both conditions and results empty, doin.. nothing"
   exit 0
 fi
-echo ${event_parsed[0]},${event_parsed[1]}
+num_of_and_conditions=$(echo $conditions | grep -o "&&" | wc -l)
+num_of_or_conditions=$(echo $conditions | grep -o "||" | wc -l)
+total_conditions=$(( num_of_and_conditions + num_of_or_conditions + 1 ))
+for i in {1..$total_conditions}
+do
+  conditions=$(parse_first_key_value_pair_from_string "$conditions")
+  echo $conditions
+  #echo $key_value_pair
+done
