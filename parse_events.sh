85b82ab9214fc49169279996648cc520c881a2fe
Suraj <me.suraj.pandey@gmail.com>
2020-07-26 17:25:38 -0400

    Still adding functionality to parse json of k8s resources

--- parse_events.sh
+++ parse_events.sh
@@ -4,6 +4,7 @@ resource_type=$2
 cur_idx_resource=$3
 resource_name=$4
 event=$5
+last_condition_operator="&&"
 
 parse_first_key_value_pair_from_string() {
   input_string=$1
@@ -12,16 +13,57 @@ parse_first_key_value_pair_from_string() {
   echo "$input_string"
 }
 
-parse_conditions() {
-  condition_string=$1
+parse_condition_if_type_is_definition() {
+  condtion=$1
+  condition_value=$2
+  condition=$(echo $condition | cut -d'.' -f3-)
   IFS_BACKUP=$IFS
   IFS='.'
-  read -a condition_key <<< "$condition_string"
+  read -a condition <<< "$condition"
+  IFS=$IFS_BACKUP
+  last_condition=${condition[-1]}
+  for each_condition in ${conditions[@]}; do
+   if [[ $each_condition != "*" ]]; then
+     jq_command=$(echo 'tostream | select(length == 2 and .[1] == "$condition_value" ) as [$p,$v] | $p | index("$each_condition")' | sed -e "s/\$condition_value/$condition_value/g" | sed -e "s/\$each_condition/$each_condition/g")
+     idx=$(echo $resource_json_output | jq '$jq_command') 
+     if [[ $idx == null ]]; then
+       echo "Event condition $each_condition did not match anything defined in the $resource_type resource json"
+       exit 1
+     fi
+   else
+     jq_command=$(echo 'tostream | select(length == 2 and .[1] == "$condition_value" ) as [$p,$v] | $p | index("$last_condition")' | sed -e "s/\$condition_value/$condition_value/g" | sed -e "s/\$last_condition/$last_condition/g")
+     idx=$(echo $resource_json_output | jq '$jq_command') 
+     if [[ $idx == null ]]; then
+       echo "Event condition $last_condition did not match anything defined in the $resource_type resource json"
+       exit 1
+     fi
+   fi 
+  done
+}
+
+parse_conditions() {
+  condition_string=$1
+  condition=$(echo $condition_string | cut -d '=' -f1)
+  condition_value=$(echo $condition_string | cut -d '=' -f2)
+  IFS_BACKUP=$IFS
+  IFS='.'
+  read -a condition_key <<< "$condition"
   IFS=$IFS_BACKUP
   if [[ ${condition_key[0]} != "event" ]]; then
-    echo "Condition string not correct, exiting.."
+    echo "Condition string not correct, must start with event, exiting.."
     exit 1
   fi
+
+  if [[ ${condition_key[1]} != "condition" ]]; then
+    echo "Condition string not correct, must start with condition, exiting.."
+  fi
+  
+  if [[ ${condition_key[2]} = "definition" ]]; then
+    parse_condition_if_type_is_defintion "$condition" "$condition_value"
+  fi 
+  #.. | .suraj? | length
+  #tostream | select(.[0][-1]=="a") as [$p,$v] | $p
+  #tostream | select(length == 2 and .[1]=="b") as [$p,$v] | $p
   #walk( if type == "object" then with_entries( select(.key == "a") ) else . end ) | .[0] | length 
 }
 
@@ -42,6 +84,7 @@ total_conditions=$(( num_of_and_conditions + num_of_or_conditions + 1 ))
 for i in {1..$total_conditions}
 do
   conditions=$(parse_first_key_value_pair_from_string "$conditions")
+  parse_conditions "$key_value_pair"
   echo $conditions
   #echo $key_value_pair
 done
